7d8bc1c25920afc5bea3ecca1259ce2fb8860146
OopsOutOfMemory <victorshengli@126.com>
2014-12-25 21:39:12 +0800

    update

--- src/main/scala/com/shengli/spark/hbase/package.scala
+++ src/main/scala/com/shengli/spark/hbase/package.scala
@@ -1,17 +1,6 @@
 /*
- * Copyright 2014 Databricks
+ * Copyright 2014 Sheng,Li
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package com.shengli.spark
 
@@ -19,16 +8,6 @@ import org.apache.spark.sql.{SQLContext, SchemaRDD}
 import scala.collection.immutable.HashMap
 
 package object hbase {
-  /**
-   *
-   *  CREATE TEMPORARY TABLE hbaseTable
-      USING com.shengli.spark.hbase
-      OPTIONS (
-        registerTableSchema   '(rowkey string, value string)',
-        externalTableName    'test',
-        externalTableSchema '(:key, cf:a)'
-      )
-   */
 
   abstract class SchemaField
 
@@ -36,7 +15,7 @@ package object hbase {
 
   case class HBaseSchemaField(fieldName: String, fieldType: String)  extends  SchemaField
   /**
-   * Adds a method, `hbaseFile`, to SQLContext that allows reading data stored in hbase table.
+   * Adds a method, `hbaseTable`, to SQLContext that allows reading data stored in hbase table.
    */
   implicit class HBaseContext(sqlContext: SQLContext) {
     def hbaseTable(registerTableSchema: String, externalTableName: String, externalTableSchema: String) = {
